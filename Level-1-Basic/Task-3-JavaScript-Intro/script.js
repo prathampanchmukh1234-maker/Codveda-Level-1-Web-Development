858c52d541b572616347b4715ec5164415324c7a
Pratham Panchmukh <prathampanchmukh1234@gmail.com>
2026-01-10 13:04:04 +0530

    Updated Level-1 tasks with advanced UI, images, animations and JS validation

--- Level-1-Basic/Task-3-JavaScript-Intro/script.js
+++ Level-1-Basic/Task-3-JavaScript-Intro/script.js
@@ -1,8 +1,23 @@
-function showName() {
-    let name = document.getElementById("name").value;
-    if(name === ""){
-        alert("Please enter your name");
-    } else {
-        document.getElementById("output").innerHTML = "Hello " + name;
-    }
+document.getElementById("myForm").addEventListener("submit", function(e){
+  e.preventDefault();
+
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let course = document.getElementById("course").value;
+
+  if(name==="" || email==="" || course===""){
+    showModal("Please fill all fields.");
+  }
+  else{
+    showModal("Form Submitted Successfully!");
+  }
+});
+
+function showModal(message){
+  document.getElementById("msg").innerText = message;
+  document.getElementById("modal").style.display="flex";
+}
+
+function closeModal(){
+  document.getElementById("modal").style.display="none";
 }
